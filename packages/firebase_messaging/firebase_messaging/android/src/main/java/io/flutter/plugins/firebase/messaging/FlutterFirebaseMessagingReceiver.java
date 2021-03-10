d16340c48e1cd9bf33f2055499d154d67a4111e3
Mohamed ELTorky <mohamedtarekeltorky@gmail.com>
2021-03-10 14:22:27 +0200

    Test 1

--- packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
+++ packages/firebase_messaging/firebase_messaging/android/src/main/java/io/flutter/plugins/firebase/messaging/FlutterFirebaseMessagingReceiver.java
@@ -48,7 +48,6 @@ public class FlutterFirebaseMessagingReceiver extends BroadcastReceiver {
         new Intent(context, FlutterFirebaseMessagingBackgroundService.class);
     onBackgroundMessageIntent.putExtra(
         FlutterFirebaseMessagingUtils.EXTRA_REMOTE_MESSAGE, remoteMessage);
-    FlutterFirebaseMessagingBackgroundService.enqueueMessageProcessing(
-        context, onBackgroundMessageIntent);
+    LocalBroadcastManager.getInstance(context).sendBroadcast(onBackgroundMessageIntent);
   }
 }
